19852dd69783f1f4b6530deb03f687da34391f9f
Stan Kazanskiy <stan.kazanskiy@gmail.com>
2016-04-05 00:56:50 +0000

    update

--- firmware/particle-rf24_config.h
+++ firmware/particle-rf24_config.h
@@ -5,27 +5,59 @@
  version 2 as published by the Free Software Foundation.
  */
 
-
 #ifndef __RF24_CONFIG_H__
 #define __RF24_CONFIG_H__
 
-    /*** USER DEFINES:  ***/
-    //#define FAILURE_HANDLING
-    //#define SERIAL_DEBUG
+#if ARDUINO < 100
+#include <WProgram.h>
+#else
+#include <Arduino.h>
+#endif
 
-    /**********************/
-    #define rf24_max(a,b) (a>b?a:b)
-    #define rf24_min(a,b) (a<b?a:b)
+#include <stddef.h>
 
-    #define _BV(x) (1<<(x))
+// Stuff that is normally provided by Arduino
+#ifdef ARDUINO
+#include <SPI.h>
+#else
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+extern HardwareSPI SPI;
+#define _BV(x) (1<<(x))
+#endif
 
-    #define PRIPSTR "%s"
-    #define pgm_read_word(p) (*(p))
-    #ifdef SERIAL_DEBUG
-        #define IF_SERIAL_DEBUG(x) ({x;})
-    #else
-        #define IF_SERIAL_DEBUG(x)
-    #endif
+#undef SERIAL_DEBUG
+#ifdef SERIAL_DEBUG
+#define IF_SERIAL_DEBUG(x) ({x;})
+#else
+#define IF_SERIAL_DEBUG(x)
+#endif
 
+// Avoid spurious warnings
+#if 1
+#if ! defined( NATIVE ) && defined( ARDUINO )
+#undef PROGMEM
+#define PROGMEM __attribute__(( section(".progmem.data") ))
+#undef PSTR
+#define PSTR(s) (__extension__({static const char __c[] PROGMEM = (s); &__c[0];}))
+#endif
+#endif
+
+// Progmem is Arduino-specific
+#ifdef ARDUINO
+#include <avr/pgmspace.h>
+#define PRIPSTR "%S"
+#else
+typedef char const char;
+typedef uint16_t prog_uint16_t;
+#define PSTR(x) (x)
+#define printf_P printf
+#define strlen_P strlen
+#define PROGMEM
+#define pgm_read_word(p) (*(p)) 
+#define PRIPSTR "%s"
+#endif
 
 #endif // __RF24_CONFIG_H__
+// vim:ai:cin:sts=2 sw=2 ft=cpp
